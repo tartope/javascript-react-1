e5f57d76b63d654c2245c33eae509aedca3c776b
Tunisia Artope <tartope@xpanxion.com>
2023-05-18 12:33:29 -0400

    Finish css for exercise 2.

--- src/Student16/Ex2/Body.js
+++ src/Student16/Ex2/Body.js
@@ -1,16 +1,22 @@
 import React from 'react'
+import './Body.css'
 
 export const Body = () => {
   return (
     <div className='body'>
+      <div className='posts'>
         <h2>Posts</h2>
         <p>6/8/2022 Hello World!!!</p>
         <p>6/9/2022 I think therefore I am.</p>
         <p>6/12/2022 I need to retire.</p>
+      </div>
+      
+      <div className='groups'>
         <h2>Groups</h2>
         <p>Likes: 12 Cats</p>
         <p>Likes: 2 Dogs </p>
         <p>Likes: 0 Good Neighbors</p>
+      </div>
     </div>
   )
 }
