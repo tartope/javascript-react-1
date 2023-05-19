09d2b066bd717ede0aaf8cf69b8bb12179c2a7af
Tunisia Artope <tartope@xpanxion.com>
2023-05-19 08:38:10 -0400

    Add components for exercise 3 Tweet Storm.

---
+++ src/Student16/Ex3/Tweets.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Tweet } from './Tweet'
+
+import './Tweets.css'
+
+export const Tweets = () => {
+  
+  return (
+    <div className="Tweets">
+        <div>Tweet Storm</div>
+        <Tweet message="It's payday!!!" color="green"/>
+        <Tweet message="I just bought a new car!" color="yellow" />
+        <Tweet message="I just smashed my new car." color="red" />
+    </div>
+  )
+}
